39bc6d3c4ace6f23be37a54791f15f3b421f66c4
sudhakar-brl <sudhakar.r@bayrocklabs.com>
2026-02-18 10:39:52 +0530

    Add generated tests for PR #11

---
+++ src/test/java/com/cardoffers/oms/controller/CardNetworkControllerTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(CardNetworkController.class)
+class CardNetworkControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("should return 200 OK")
+    void shouldReturn200() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+}
